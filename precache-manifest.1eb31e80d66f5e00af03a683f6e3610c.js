6b1165fd3ea5a4c0c0088f4d4567ce04e9e1271d
mariuspara <mariusparaschiv8@gmail.com>
2019-04-14 12:55:11 +0300

    Updates

---
+++ precache-manifest.1eb31e80d66f5e00af03a683f6e3610c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mariuspara/The-Burger-Builder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d1e658516b1e286d5f5f",
+    "url": "/mariuspara/The-Burger-Builder/static/js/runtime~main.f9ca3aba.js"
+  },
+  {
+    "revision": "a242efd7d65f76df25e6",
+    "url": "/mariuspara/The-Burger-Builder/static/js/main.63da7cce.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/mariuspara/The-Burger-Builder/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a242efd7d65f76df25e6",
+    "url": "/mariuspara/The-Burger-Builder/static/css/main.981940a4.chunk.css"
+  },
+  {
+    "revision": "0ac8f3a5bab6d2c467105df616aa1b7a",
+    "url": "/mariuspara/The-Burger-Builder/index.html"
+  }
+];
